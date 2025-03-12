fd4b026ad87b0cffbb7ccc5d446a33fba7d30d76
ashokv <ashok3akr@gmail.com>
2025-03-12 18:57:57 +0530

    ansible

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-ansible-playbook -i $1, -e ansible_user=ec2-user -e ansible_password=DevOps321 roboshop.yml -e component_name=$1
+ansible-playbook -i 172.31.25.195, -e ansible_user=ec2-user -e ansible_password=DevOps321 roboshop.yml -e component_name=$1
